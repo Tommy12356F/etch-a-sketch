bc6d764f629a3b9be2f3e91878455719febcf205
Vedant <vedant.vidhani@gmail.com>
2025-12-27 03:58:57 +0530

    Added random colors on hover

--- script.js
+++ script.js
@@ -2,7 +2,11 @@ let container = document.querySelector(".container")
 for (let i = 0; i < 256; i++) {
   const square = document.createElement("div");
   square.addEventListener("mouseenter", () =>{
-    square.style.backgroundColor="Blue"
+    r=Math.floor(Math.random()*255)
+    g=Math.floor(Math.random()*255)
+    b=Math.floor(Math.random()*255)
+    square.style.backgroundColor="rgb(" + r + "," + g + "," + b + ")"
+    
   })
   
   square.classList.add("square")
@@ -13,7 +17,7 @@ for (let i = 0; i < 256; i++) {
 let reset = document.querySelector(".reset")
 reset.addEventListener("click",()=>{
     let size = prompt("Enter no: ")
-    if (size>80){
+    if (size>81){
         alert("Enter Valid no")
         return;
     }
@@ -23,7 +27,11 @@ reset.addEventListener("click",()=>{
     for (let i = 0; i < size*size; i++) {
         const square = document.createElement("div");
         square.addEventListener("mouseenter", () =>{
-            square.style.backgroundColor="Blue"
+            r=Math.floor(Math.random()*255)
+            g=Math.floor(Math.random()*255)
+            b=Math.floor(Math.random()*255)
+            square.style.backgroundColor="rgb(" + r + "," + g + "," + b + ")"
+
         })
         square.style.width=(960/size) + "px"
         square.style.height=(960/size) + "px"
